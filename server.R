91a8ff5186f68fdb8b1f8687683443d2211d68e3
ArmindoMaurits <a.r.maurits@gmail.com>
2016-10-07 14:09:58 +0200

    initial commit

---
+++ server.R
@@ -0,0 +1,77 @@
+library(shiny)
+library(leaflet)
+
+shinyServer(function(input, output) {
+  colorPalette <- c('#a50026','#d73027','#f46d43','#fdae61','#fee08b','#ffffbf','#d9ef8b','#a6d96a','#66bd63','#1a9850','#006837')
+  #To view the color palette
+  #pie(rep(1, 11), col = colorPalette)
+  wijken <- read.csv(paste(getwd(), "datasets/all_data_wijken.csv", sep="/"), sep = ";")
+  buurten <- read.csv(paste(getwd(), "datasets/all_data_buurten.csv", sep="/"), sep = ";")
+  
+  output$map <- renderLeaflet({
+    map <<- leaflet(data = wijken)
+    map <<- addTiles(map)
+    map <<- setView(map, 4.477733, 51.92442, zoom = 12)
+    map <<- addLegend(map, "bottomright", colors = rev(colorPalette), labels = 10:0,opacity = 1)
+    
+    addGeoJsonFromDatasetToMap("buurten")
+    # addGeoJsonFromDatasetToMap("wijken")
+    map  # Show the map
+  })
+  
+  output$buurtenPlot <- renderPlot({
+    barplot(buurten$veiligheidsindex_sub_norm, names=buurten$buurtnaam, las=2, col=colorPalette[buurten$veiligheidsindex_sub_norm+1], main="Veiligheidsindex per buurt", ylab="veiligheidsindex")
+    grid(nx = 0, ny=NULL)
+  })
+  #test
+})
+
+#Normalize a given column to a range from 0 to 10
+normalizeColumn <- function(column) {
+  library("scales")
+  scaled <- round(rescale(column)*10)
+}
+
+#Delfshaven - http://thesaurus.erfgeo.nl/pit/?id=gemeentegeschiedenis-geometries/Delfshaven-1812
+addGeoJsonFromDatasetToMap<- function(datasetName){
+  
+  if(datasetName == "wijken"){
+    for(wijknummer in wijken$cbs_wijknummer){
+      wijkenFolder <- paste(getwd(), "/geojsons/wijken/", sep = "")
+      fileName <- paste(wijknummer, ".json", sep="")
+      
+      json <- readLines(paste(wijkenFolder, fileName, sep="")) %>% paste(collapse = "\n")
+      
+      map <<- addGeoJSON(map, json, weight = 2, color = "#000", fill = T)
+    }
+  }else{
+    for(buurtNummer in buurten$cbs_buurtnummer){
+      buurtenFolder <- paste(getwd(), "/geojsons/buurten/", sep = "")
+      fileName <- paste(buurtNummer, ".json", sep="")
+      
+      json <- readLines(paste(buurtenFolder, fileName, sep="")) %>% paste(collapse = "\n")
+      map <<- addGeoJSON(map, json, weight = 2, color = colorPalette[buurten$veiligheidsindex_sub_norm[buurten$cbs_buurtnummer == buurtNummer]+1], fillColor =  colorPalette[buurten$veiligheidsindex_sub_norm[buurten$cbs_buurtnummer == buurtNummer]+1] , fill = T, stroke=T,opacity = 1, fillOpacity=0.75)
+    }
+  }
+}
+
+#
+#tempDataFrame <- data.frame(buurten$cbs_buurtnummer, normalizeColumn(buurten$aantal_hav.vwoschool))
+#colnames(tempDataFrame) <- c("cbs_buurtnummer", "aantal_hav.vwoschool_norm")
+#buurten <- merge(buurten, tempDataFrame, by.y = "cbs_buurtnummer")
+#write.csv2(buurten, file = paste(getwd(), "/datasets/all_data_buurten.csv", sep=""), row.names = F)
+#
+
+addGeoJsonByNumberAndCategory <- function(nummer, category){
+  if(category == "wijken"){
+    folderLocation <- paste(getwd(), "/geojsons/wijken/", sep = "")  
+  }else{
+    folderLocation <- paste(getwd(), "/geojsons/buurten/", sep = "")
+  }
+  
+  fileName <- paste(nummer, ".json", sep="")
+  fileLocation <-paste(folderLocation, fileName, sep="")
+  json <- readLines(fileLocation) %>% paste(collapse = "\n")
+  
+  return(json)
+}
---
+++ ui.R
@@ -0,0 +1,45 @@
+library(shiny)
+library(leaflet)
+
+shinyUI(fluidPage(
+  
+  # Application title
+  titlePanel("Neighbourhood Quality Map"),
+  fluidRow(
+    column(6,
+           h4("Persoonlijke informatie"),
+           sliderInput('age', 'leeftijd', min=0, max=100, value=25),
+           selectInput('status', 'status', c("student", "werkend", "werkloos", "gepensioneerd"))
+           
+    ),
+    column(6,
+           h4("Eisen"),
+           checkboxInput('airQuality', 'Luchtkwaliteit'),
+           checkboxInput('sound', 'Geluid in de omgeving'),
+           checkboxInput('safetyIndex', 'Veiligheidsindex'),
+           checkboxInput('parkingLots', 'Parkeerplaatsen'),
+           checkboxInput('services', 'Voorzieningen')
+    ),
+    column(12, align="center",
+           actionButton('search', 'Zoeken'))
+  ),
+  hr(),
+  fluidRow(
+    column(12,
+           leafletOutput("map", width="100%", height="500px"),
+           absolutePanel(top = 10, right = 10, background="red",
+                         checkboxInput("legend", "Toon legenda", F)
+           )
+    )
+  ),
+  fluidRow(
+    column(12,
+           plotOutput("buurtenPlot")
+    )
+  ),
+  fluidRow(
+    column(12,
+           plotOutput("wijkenPlot")
+    )
+  )
+))
