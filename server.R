b820645841b67d58827fc017d5285b1eb8956789
Armindo Maurits <0882940@hr.nl>
2016-10-10 12:39:46 +0200

    Changed method to plot buurten with colour of column

--- server.R
+++ server.R
@@ -2,11 +2,7 @@ library(shiny)
 library(leaflet)
 
 shinyServer(function(input, output) {
-  colorPalette <- c('#a50026','#d73027','#f46d43','#fdae61','#fee08b','#ffffbf','#d9ef8b','#a6d96a','#66bd63','#1a9850','#006837')
-  #To view the color palette
-  #pie(rep(1, 11), col = colorPalette)
-  wijken <- read.csv(paste(getwd(), "datasets/all_data_wijken.csv", sep="/"), sep = ";")
-  buurten <- read.csv(paste(getwd(), "datasets/all_data_buurten.csv", sep="/"), sep = ";")
+  initVariables()
   
   output$map <- renderLeaflet({
     map <<- leaflet(data = wijken)
@@ -14,8 +10,7 @@ shinyServer(function(input, output) {
     map <<- setView(map, 4.477733, 51.92442, zoom = 12)
     map <<- addLegend(map, "bottomright", colors = rev(colorPalette), labels = 10:0,opacity = 1)
     
-    addGeoJsonFromDatasetToMap("buurten")
-    # addGeoJsonFromDatasetToMap("wijken")
+    plotBuurtenWithColumn(buurten$veiligheidsindex_ob_norm)
     map  # Show the map
   })
   
@@ -23,7 +18,7 @@ shinyServer(function(input, output) {
     barplot(buurten$veiligheidsindex_sub_norm, names=buurten$buurtnaam, las=2, col=colorPalette[buurten$veiligheidsindex_sub_norm+1], main="Veiligheidsindex per buurt", ylab="veiligheidsindex")
     grid(nx = 0, ny=NULL)
   })
-  #test
+  
 })
 
 #Normalize a given column to a range from 0 to 10
@@ -32,26 +27,14 @@ normalizeColumn <- function(column) {
   scaled <- round(rescale(column)*10)
 }
 
-#Delfshaven - http://thesaurus.erfgeo.nl/pit/?id=gemeentegeschiedenis-geometries/Delfshaven-1812
-addGeoJsonFromDatasetToMap<- function(datasetName){
-  
-  if(datasetName == "wijken"){
-    for(wijknummer in wijken$cbs_wijknummer){
-      wijkenFolder <- paste(getwd(), "/geojsons/wijken/", sep = "")
-      fileName <- paste(wijknummer, ".json", sep="")
-      
-      json <- readLines(paste(wijkenFolder, fileName, sep="")) %>% paste(collapse = "\n")
-      
-      map <<- addGeoJSON(map, json, weight = 2, color = "#000", fill = T)
-    }
-  }else{
-    for(buurtNummer in buurten$cbs_buurtnummer){
-      buurtenFolder <- paste(getwd(), "/geojsons/buurten/", sep = "")
-      fileName <- paste(buurtNummer, ".json", sep="")
-      
-      json <- readLines(paste(buurtenFolder, fileName, sep="")) %>% paste(collapse = "\n")
-      map <<- addGeoJSON(map, json, weight = 2, color = colorPalette[buurten$veiligheidsindex_sub_norm[buurten$cbs_buurtnummer == buurtNummer]+1], fillColor =  colorPalette[buurten$veiligheidsindex_sub_norm[buurten$cbs_buurtnummer == buurtNummer]+1] , fill = T, stroke=T,opacity = 1, fillOpacity=0.75)
-    }
+#Plots all buurten with the colour of a given column from the buurten data.frame
+plotBuurtenWithColumn <- function(column){
+  for(buurtNummer in buurten$cbs_buurtnummer){
+    buurtenFolder <- paste(getwd(), "/geojsons/buurten/", sep = "")
+    fileName <- paste(buurtNummer, ".json", sep="")
+    
+    json <- readLines(paste(buurtenFolder, fileName, sep="")) %>% paste(collapse = "\n")
+    map <<- addGeoJSON(map, json, weight = 2, color = colorPalette[column[buurten$cbs_buurtnummer == buurtNummer]+1], fillColor =  colorPalette[column[buurten$cbs_buurtnummer == buurtNummer]+1] , fill = T, stroke=T,opacity = 1, fillOpacity=0.75)
   }
 }
 
@@ -74,4 +57,38 @@ addGeoJsonByNumberAndCategory <- function(nummer, category){
   json <- readLines(fileLocation) %>% paste(collapse = "\n")
   
   return(json)
+}
+
+#Delfshaven - http://thesaurus.erfgeo.nl/pit/?id=gemeentegeschiedenis-geometries/Delfshaven-1812
+addGeoJsonFromDatasetToMap<- function(datasetName, columnName){
+  
+  if(datasetName == "wijken"){
+    for(wijknummer in wijken$cbs_wijknummer){
+      wijkenFolder <- paste(getwd(), "/geojsons/wijken/", sep = "")
+      fileName <- paste(wijknummer, ".json", sep="")
+      
+      json <- readLines(paste(wijkenFolder, fileName, sep="")) %>% paste(collapse = "\n")
+      
+      map <<- addGeoJSON(map, json, weight = 2, color = "#000", fill = T)
+    }
+  }else{
+    
+    for(buurtNummer in buurten$cbs_buurtnummer){
+      buurtenFolder <- paste(getwd(), "/geojsons/buurten/", sep = "")
+      fileName <- paste(buurtNummer, ".json", sep="")
+      
+      json <- readLines(paste(buurtenFolder, fileName, sep="")) %>% paste(collapse = "\n")
+      map <<- addGeoJSON(map, json, weight = 2, color = colorPalette[buurten$veiligheidsindex_sub_norm[buurten$cbs_buurtnummer == buurtNummer]+1], fillColor =  colorPalette[buurten$veiligheidsindex_sub_norm[buurten$cbs_buurtnummer == buurtNummer]+1] , fill = T, stroke=T,opacity = 1, fillOpacity=0.75)
+    }
+  }
+}
+
+#Loads all variables into the workspace.
+initVariables <- function(){
+  colorPalette <- c('#a50026','#d73027','#f46d43','#fdae61','#fee08b','#ffffbf','#d9ef8b','#a6d96a','#66bd63','#1a9850','#006837')
+  #To view the color palette
+  #pie(rep(1, 11), col = colorPalette)
+  
+  wijken <- read.csv(paste(getwd(), "datasets/all_data_wijken.csv", sep="/"), sep = ";")
+  buurten <- read.csv(paste(getwd(), "datasets/all_data_buurten.csv", sep="/"), sep = ";")
 }
