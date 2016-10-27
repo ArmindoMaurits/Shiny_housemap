4e8ed514f88f72e72797506135e7b0ef099c801a
rberger91 <robbinberger@gmail.com>
2016-10-27 13:03:38 +0200

    Pages gemaakt
    Pages gemaakt en aan 't kloten gegaan met de invulling van de main page

--- ui.R
+++ ui.R
@@ -8,35 +8,42 @@ sidebar <- dashboardSidebar(
   sidebarMenu(
     menuItem("Start", tabName = "startPage", icon = icon("dashboard")),
     menuItem("Map", tabName = "mapPage", icon = icon("dashboard"))
+           
   ),
-  width=300,
+  width=300, 
   fluidRow(
     column(12,
       h4("Kolom"),
       selectInput('selectedDataset', 'Dataset column', choices = names(buurten), selected = "veiligheidsindex_sub_norm")
     )
-  ),
-  sidebarMenu(
-    menuItem("Categorien",
-             checkboxInput("age", "Leeftijd"),
-             checkboxInput("origin", "Herkomst"),
-             checkboxInput("services", "Voorzieningen"),
-             checkboxInput("schools", "Scholen"),
-             checkboxInput("publicTransport", "Openbaarvervoer"),
-             checkboxInput("safetyIndex", "Veiligheidsindex")
+  )#,
+  #sidebarMenu(
+    #menuItem("Categorien",
+             
     )
-  )
-)
+
 
 body <- dashboardBody(
-  fluidRow(
+  tabItems(
+    tabItem(tabName = "mapPage",
+    fluidRow(
     tags$style(type = "text/css", "#map {height: calc(100vh - 80px) !important;}"),
     leafletOutput("map"),
     absolutePanel(top = 10, right = 10, background="red",
                  checkboxInput("legend", "Toon legenda", F)
     )
   )
+),
+  tabItem(tabName = "startPage", h2("Dit is een pagina in ontwikkeling"),
+          fluidRow(
+            box(title = "Kies uw categorie", collapsible = T),
+            box(title = "test 2", collapsible = T),
+            box(title = "test 3", collapsible = T),
+            box(title = "test 4", collapsible = T)
+            ))
 )
+)
+
 
 
 dashboardPage(header, sidebar, body)
